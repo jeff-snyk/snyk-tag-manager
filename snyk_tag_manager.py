dfb6f1ae2901e78eb8a440f457908d315098d7ae
Jeff B <jeff.b@runbox.com>
2023-04-26 14:32:01 -0700

    changed GUI library to PyQt

--- snyk_tag_manager.py
+++ snyk_tag_manager.py
@@ -1,117 +1,158 @@
-import tkinter as tk
-from tkinter import ttk
-import tkinter.messagebox as messagebox
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QGridLayout, QLabel, QLineEdit, QPushButton, QTreeView, QVBoxLayout, QWidget, QMessageBox
+from PyQt6.QtGui import QStandardItemModel, QStandardItem
+from PyQt6.QtCore import QModelIndex, Qt
 import requests
 import json
 
-def fetch_tags(api_key, group_id):
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"token {api_key}"
-    }
-    url = f"https://api.snyk.io/v1/group/{group_id}/tags"
-    response = requests.get(url, headers=headers)
-    return response.json()
+class SnykTagManager(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-def list_tags(page=0):
-    api_key = api_key_entry.get()
-    group_id = group_id_entry.get()
+        self.current_page = 0
+        self.init_ui()
 
-    if not api_key or not group_id:
-        messagebox.showerror("Error", "Please provide both API token and Group ID.")
-        return
+    def init_ui(self):
+        self.setWindowTitle("Snyk Tag Manager")
 
-    tag_data = fetch_tags(api_key, group_id)
+        # Create central widget and layout
+        central_widget = QWidget()
+        main_layout = QVBoxLayout()
+        central_widget.setLayout(main_layout)
+        self.setCentralWidget(central_widget)
 
-    tags_treeview.delete(*tags_treeview.get_children())  # Clear the treeview before displaying new tags
-    start_index = page * 25
-    end_index = start_index + 25
-    for tag in tag_data['tags'][start_index:end_index]:
-        tags_treeview.insert("", tk.END, text="", values=(f"{tag['key']}: {tag['value']}", "Delete"))
+        # Create input layout
+        input_layout = QGridLayout()
+        main_layout.addLayout(input_layout)
 
-    # Update navigation buttons state
-    back_button.config(state=tk.NORMAL if page > 0 else tk.DISABLED)
-    next_button.config(state=tk.NORMAL if end_index < len(tag_data['tags']) else tk.DISABLED)
+        # API key input
+        api_key_label = QLabel("Snyk API Token:")
+        input_layout.addWidget(api_key_label, 0, 0)
+        self.api_key_entry = QLineEdit()
+        self.api_key_entry.setFixedWidth(300)
+        input_layout.addWidget(self.api_key_entry, 0, 1)
 
+        # Group ID input
+        group_id_label = QLabel("Group ID:")
+        input_layout.addWidget(group_id_label, 1, 0)
+        self.group_id_entry = QLineEdit()
+        self.group_id_entry.setFixedWidth(300)
+        input_layout.addWidget(self.group_id_entry, 1, 1)
+ 
+        # List tags button
+        list_tags_button = QPushButton("List Tags")
+        list_tags_button.clicked.connect(self.list_tags)
+        main_layout.addWidget(list_tags_button)
 
-def delete_tag(api_key, group_id, tag_key, tag_value):
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"token {api_key}"
-    }
-    url = f"https://api.snyk.io/v1/group/{group_id}/tags/delete"
-    payload = {
-        "key": tag_key,
-        "value": tag_value,
-        "force": False
-    }
-    response = requests.post(url, headers=headers, json=payload)
-    return response.status_code, response.json()
+        # Treeview
+        self.tags_treeview = QTreeView()
+        self.tags_treeview.setHeaderHidden(False)
+        self.tags_treeview.setFixedHeight(500)
+        main_layout.addWidget(self.tags_treeview)
 
-def on_treeview_click(event):
-    item = tags_treeview.identify("item", event.x, event.y)
-    column = tags_treeview.identify("column", event.x, event.y)
-    if column == "#2":
-        tag = tags_treeview.item(item, "values")[0]
-        tag_key, tag_value = tag.split(": ")
-        api_key = api_key_entry.get()
-        group_id = group_id_entry.get()
-        response_status, response_json = delete_tag(api_key, group_id, tag_key, tag_value)
+        self.model = QStandardItemModel(0, 2)
+        self.model.setHeaderData(0, Qt.Orientation.Horizontal, "Tag")
+        self.model.setHeaderData(1, Qt.Orientation.Horizontal, "Delete")
+        self.tags_treeview.setModel(self.model)
+        self.tags_treeview.setColumnWidth(0, 300)
+        self.tags_treeview.setColumnWidth(1, 75)
+        self.tags_treeview.clicked.connect(self.on_treeview_click)
 
-        if response_status == 200:
-            tags_treeview.delete(item)
-        else:
-            if response_status == 403:
-                message = f"Error deleting tag:\n\n{response_json['message']}. \n\nThis likely means there are projects using this tag."
+        # Navigation buttons
+        nav_layout = QGridLayout()
+        main_layout.addLayout(nav_layout)
+
+        self.back_button = QPushButton("Back")
+        self.back_button.clicked.connect(self.go_back)
+        self.back_button.setEnabled(False)
+        nav_layout.addWidget(self.back_button, 0, 0)
+
+        self.next_button = QPushButton("Next")
+        self.next_button.clicked.connect(self.go_next)
+        self.next_button.setEnabled(False)
+        nav_layout.addWidget(self.next_button, 0, 1)
+
+        # Quit button
+        quit_button = QPushButton("Quit")
+        quit_button.clicked.connect(self.close)
+        main_layout.addWidget(quit_button)
+
+    def fetch_tags(self, api_key, group_id):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"token {api_key}"
+        }
+        url = f"https://api.snyk.io/v1/group/{group_id}/tags"
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+    def list_tags(self, page=0):
+        api_key = self.api_key_entry.text()
+        group_id = self.group_id_entry.text()
+
+        if not api_key or not group_id:
+            QMessageBox.critical(self, "Error", "Please provide both API token and Group ID.")
+            return
+
+        tag_data = self.fetch_tags(api_key, group_id)
+
+        self.model.removeRows(0, self.model.rowCount())  # Clear the treeview before displaying new tags
+        start_index = page * 25
+        end_index = start_index + 25
+        for tag in tag_data['tags'][start_index:end_index]:
+            tag_item = QStandardItem(f"{tag['key']}: {tag['value']}")
+            delete_item = QStandardItem("Delete")
+            self.model.appendRow([tag_item, delete_item])
+
+        # Update navigation buttons state
+        self.back_button.setEnabled(page > 0)
+        self.next_button.setEnabled(end_index < len(tag_data['tags']))
+
+    def delete_tag(self, api_key, group_id, tag_key, tag_value):
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"token {api_key}"
+        }
+        url = f"https://api.snyk.io/v1/group/{group_id}/tags/delete"
+        payload = {
+            "key": tag_key,
+            "value": tag_value,
+            "force": False
+        }
+        response = requests.post(url, headers=headers, json=payload)
+        return response.status_code, response.json()
+
+    def on_treeview_click(self, index: QModelIndex):
+        row = index.row()
+        column = index.column()
+
+        if column == 1:
+            tag = self.model.item(row, 0).text()
+            tag_key, tag_value = tag.split(": ")
+
+            api_key = self.api_key_entry.text()
+            group_id = self.group_id_entry.text()
+            response_status, response_json = self.delete_tag(api_key, group_id, tag_key, tag_value)
+
+            if response_status == 200:
+                self.model.removeRow(row)
             else:
-                message = f"Failed to delete tag. Error code: {response_status}"
-            messagebox.showerror("Error", message)
+                if response_status == 403:
+                    message = f"Error deleting tag:\n\n{response_json['message']}. \n\nThis likely means there are projects using this tag."
+                else:
+                    message = f"Failed to delete tag. Error code: {response_status}"
+                QMessageBox.critical(self, "Error", message)
 
-def go_back():
-    global current_page
-    current_page -= 1
-    list_tags(current_page)
+    def go_back(self):
+        self.current_page -= 1
+        self.list_tags(self.current_page)
 
-def go_next():
-    global current_page
-    current_page += 1
-    list_tags(current_page)
+    def go_next(self):
+        self.current_page += 1
+        self.list_tags(self.current_page)
 
-root = tk.Tk()
-root.title("Snyk Tag Manager")
-
-current_page = 0
-
-api_key_label = tk.Label(root, text="Snyk API Token:")
-api_key_label.grid(row=0, column=0, sticky="w", padx=5, pady=5)
-api_key_entry = tk.Entry(root, width=40)
-api_key_entry.grid(row=0, column=1, padx=5, pady=5)
-
-group_id_label = tk.Label(root, text="Group ID:")
-group_id_label.grid(row=1, column=0, sticky="w", padx=5, pady=5)
-group_id_entry = tk.Entry(root, width=40)
-group_id_entry.grid(row=1, column=1, padx=5, pady=5)
-
-list_tags_button = tk.Button(root, text="List Tags", command=list_tags)
-list_tags_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
-
-tags_treeview = ttk.Treeview(root, columns=("Tag", "Delete"), show="headings", height=28)
-tags_treeview.heading("Tag", text="Tag")
-tags_treeview.column("Tag", width=300)
-tags_treeview.heading("Delete", text="Delete")
-tags_treeview.column("Delete", width=75)
-tags_treeview.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
-
-tags_treeview.bind("<1>", on_treeview_click)
-
-back_button = tk.Button(root, text="Back", command=go_back, state=tk.DISABLED)
-back_button.grid(row=4, column=0, padx=5, pady=5)
-
-next_button = tk.Button(root, text="Next", command=go_next, state=tk.DISABLED)
-next_button.grid(row=4, column=1, padx=5, pady=5)
-
-quit_button = tk.Button(root, text="Quit", command=root.quit)
-quit_button.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
-
-root.mainloop()
-
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    snyk_tag_manager = SnykTagManager()
+    snyk_tag_manager.show()
+    sys.exit(app.exec())
